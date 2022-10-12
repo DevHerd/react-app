435bf56e58d1c93f711dd4a02c6714e535a5e0a7
Carlo Menorias <carlo.menorias@stok.se>
2022-10-12 17:53:40 +0800

    fix: adjusting component name

--- src/Components/Counter/Counter.js
+++ src/Components/Counter/Counter.js
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import styles from './Toggle.module.scss';
+import styles from './Counter.module.scss';
 
 const Counter = () => {
   const [clickCount, setClickCount] = useState(0);
--- src/Components/layout.js
+++ src/Components/layout.js
@@ -1,5 +1,5 @@
 import Header from "./Header/Header";
-import Counter from "./Counter/toggle";
+import Counter from "./Counter/Counter";
 
 // eslint-disable-next-line import/prefer-default-export
 export { Header, Counter };
