37b39c00cddbb66c85c9c3d41b5f8958fa91eced
John Carlo Menorias <47134598+CarloStokMnl@users.noreply.github.com>
2022-10-13 12:58:05 +0800

    Experimental counter (#10)
    * feat: adding indent and component
    
    * fix: adjusting component name
    
    * feat: adding decrease and reset button
    
    * fix: changing button labels

--- src/Components/Counter/Counter.js
+++ src/Components/Counter/Counter.js
@@ -5,14 +5,20 @@ const Counter = () => {
   const [clickCount, setClickCount] = useState(0);
   const onClickCounterHandler = () => setClickCount(clickCount + 1);
   const onClickResetHandler = () => setClickCount(0);
+  const onClickDecreaseCounterHandler = () => setClickCount(clickCount - 1);
   
   return (
     <section>
         <h2>Counter</h2>
-        <div>How many times i was clicked: {clickCount}</div>
+        <div>I was clicked: {clickCount}</div>
         <div className={styles.buttonWrapper}>
-          <button type="button" className={styles.counterButton} onClick={onClickCounterHandler}>Counter Click</button>
-          <button type="button" className={styles.counterButton} onClick={onClickResetHandler}>Reset Click</button>
+          <button type="button" className={styles.counterButton} onClick={onClickCounterHandler}>{clickCount > 0 ? 'Increase' : 'Counter'}</button>
+          {clickCount > 0 && (
+            <>
+              <button type="button" className={styles.counterButton} onClick={onClickDecreaseCounterHandler}>Decrease</button>
+              <button type="button" className={styles.counterButton} onClick={onClickResetHandler}>Reset</button>
+            </>
+          )}
         </div>
     </section>
   );
