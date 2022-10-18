501b115525f62662d7276002f508f328528b0986
John Carlo Menorias <47134598+CarloStokMnl@users.noreply.github.com>
2022-10-18 17:48:20 +0800

    fix: adding exception eslint

--- src/Components/Timeout/Timeout.js
+++ src/Components/Timeout/Timeout.js
@@ -19,6 +19,7 @@ const Timeout = () => {
 
   useEffect(() => {
     setTimeout(setTimeoutValue(timeoutValue + 1), 1000);
+  // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
 
   useEffect(() => {
